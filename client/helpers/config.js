f1d5efa4ce38a8523d0eac5d9324376cbe726724
Angel Qian <angel.qian@calix.com>
2017-11-03 16:32:13 -0700

    user count

---
+++ client/helpers/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by aqian on 11/3/17.
+ */
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+});
---
+++ client/templates/application/layout.js
@@ -0,0 +1,6 @@
+/**
+ * Created by aqian on 11/3/17.
+ */
+Template.layout.helpers({
+    pageTitle: function() { return Session.get('pageTitle'); }
+});
