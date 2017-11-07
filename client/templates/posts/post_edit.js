6bcd1fbcc4c27185564762834a61af566adc8018
Angel Qian <angel.qian@calix.com>
2017-11-07 11:56:03 -0800

    CUD

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,32 @@
+/**
+ * Created by aqian on 11/7/17.
+ */
+Template.postEdit.events({
+    'submit form': function(e) {
+        e.preventDefault();
+        var currentPostId = this._id;
+        var postProperties = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val(),
+            _id: currentPostId
+        };
+        //Posts.update(currentPostId, {$set: postProperties}, function(error) {
+        Meteor.call('postUpdate', postProperties, function(error, result){
+            if (error) {
+                // display the error to the user
+                alert(error.reason);
+            } else {
+
+            }
+        });
+        Router.go('postPage', {_id: currentPostId});
+    },
+    'click .delete': function(e) {
+        e.preventDefault();
+        if (confirm("Delete this post?")) {
+            var currentPostId = this._id;
+            Posts.remove(currentPostId);
+            Router.go('postsList');
+        }
+    }
+});
--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,9 @@
  * Created by aqian on 11/3/17.
  */
 Template.postItem.helpers({
+    ownPost: function() {
+        return this.userId === Meteor.userId();
+    },
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
