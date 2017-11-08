b0201f8ae3ab02b9474e11c2906195ddf106977d
Angel Qian <angel.qian@calix.com>
2017-11-08 12:50:59 -0800

    Validate create

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,6 +1,17 @@
 /**
  * Created by AngelQian on 11/5/17.
  */
+Template.postSubmit.onCreated(function() {
+    Session.set('postSubmitErrors', {});
+});
+Template.postSubmit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+    }
+});
 Template.postSubmit.events({
     'submit form': function (e) {
         e.preventDefault();
@@ -10,8 +21,11 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         };
-        //post._id = Posts.insert(post);
-        //Router.go('postPage', post);
+
+        var errors = validatePost(post);
+        if (errors.title || errors.url)
+            return Session.set('postSubmitErrors', errors);
+
         Meteor.call('postInsert', post, function(error, result) {
             // display the error to the user and abort
             if (error)
